544ca13f7664c551e8e4107be612a20fde341699
nandamacedo <30451636+nandamacedo@users.noreply.github.com>
2017-08-02 23:10:36 -0300

    Add files via upload

---
+++ weathersearch.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<link rel="stylesheet" type="text/css" href="bootstrap.css">
+<link rel="stylesheet" type="text/css"  href="estilo.css" />
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> Weather Search</title>
+</head>
+<body>
+
+<br><br><br>
+
+<div class="container">
+
+<form action="weather.php" method="post">
+  <h1>Weather Search: </h1>
+
+  <br><br>
+
+  <section>
+  <section class="row formAdiconarDados">
+
+	<section class="col-md-4">
+    	<div class="input-group">
+  			<span class="input-group-addon" id="basic-addon1">Cidade</span>
+ 			<input id="sala_aula" type="text" class="form-control" name=cidade aria-describedby="basic-addon1" value="">
+		</div>
+    </section>
+
+
+
+
+
+  <input class="btn btn-danger" type=submit value="Pequisar"> </form>
+  </section>
+
+  </section>
+
+  <br><br><br><br>
+
+    <div class="footer">
+
+         <p>Trabalho Prático I de Sistemas Distribuídos - Web Services</p>
+
+     </div>
+
+  </div>
+
+</body>
+
+</html>
